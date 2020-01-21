c762b7979b9add698f75ff99543d3a87ba097c1e
sandeep chahal <sandeepchahal573@gmail.com>
2020-01-21 19:46:55 +0530

    structured register.js

--- src/Components/Auth/Register.js
+++ src/Components/Auth/Register.js
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./Auth.style.scss";
 
-const Register = () => (
-  <div>
-    Register<Link to="/login">Login</Link>
-  </div>
-);
+const Register = props => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  handleSubmit = () => {};
+
+  return (
+    <div className="register">
+      <h2>Create an account</h2>
+      <form onSubmit={handleSubmit}>
+        <label for="email">EMAIL</label>
+        <input
+          id="email"
+          type="email"
+          autoComplete="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+
+        <label for="username">USERNAME</label>
+        <input
+          id="username"
+          type="text"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+
+        <label for="password">PASSWORD</label>
+        <input
+          id="password"
+          type="password"
+          autoComplete="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Continue</button>
+      </form>
+      <Link to="/login">Already have an account?</Link>
+    </div>
+  );
+};
 
 export default Register;
