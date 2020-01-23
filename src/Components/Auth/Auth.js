f2d63325a4071a0ec12a454dcbaa1529b6ce6323
sandeep chahal <sandeepchahal573@gmail.com>
2020-01-23 17:38:47 +0530

    convert auth.js from funtional to class based component

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -6,27 +6,40 @@ import { Route, Switch } from "react-router-dom";
 import { withRouter } from "react-router";
 import "./Auth.style.scss";
 
-const Auth = ({ location }) => (
-  <div className="auth">
-    <div className="wrapper">
-      <TransitionGroup>
-        <CSSTransition key={location.key} timeout={500} classNames="alert">
-          <Switch>
-            <Route exact path="/login">
-              <Login />
-            </Route>
-            <Route exact path="/register">
-              <Register />
-            </Route>
-            {/* in case if nothing renders and user is not logged in */}
-            <Route path="/*">
-              <Login />
-            </Route>
-          </Switch>
-        </CSSTransition>
-      </TransitionGroup>
-    </div>
-  </div>
-);
+class Auth extends React.Component {
+  componentDidMount() {}
+
+  login = (email, password) => {};
+  register = (email, password, username) => {};
+
+  render() {
+    return (
+      <div className="auth">
+        <div className="wrapper">
+          <TransitionGroup>
+            <CSSTransition
+              key={this.props.location.key}
+              timeout={500}
+              classNames="alert"
+            >
+              <Switch>
+                <Route exact path="/login">
+                  <Login />
+                </Route>
+                <Route exact path="/register">
+                  <Register />
+                </Route>
+                {/* in case if nothing renders and user is not logged in */}
+                <Route path="/*">
+                  <Login />
+                </Route>
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default withRouter(Auth);
