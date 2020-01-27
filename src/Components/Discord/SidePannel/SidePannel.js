06116aa3bff933cdbb63921c73c767ee191a4208
sandeep chahal <sandeepchahal573@gmail.com>
2020-01-27 10:22:49 +0530

    converted sidepannel to class based component

--- src/Components/Discord/SidePannel/SidePannel.js
+++ src/Components/Discord/SidePannel/SidePannel.js
@@ -4,29 +4,30 @@ import "./SidePannel.scss";
 import AddModal from "../AddModal/AddModal";
 import uuidv4 from "uuid/v4";
 
-const SidePannel = ({
-  firebase,
-  user,
-  loadServer,
-  joinedServers,
-  selectServer
-}) => {
-  const [showModal, setShowModal] = useState(false);
-  const [percentage, setPercentage] = useState(0);
-  const [createStatus, setStatus] = useState("");
-  const serverRef = firebase.database().ref("servers");
-  const storage = firebase.storage().ref("serversIcon");
+// firebase,
+// user,
+// loadServer,
+// joinedServers,
+// selectServer
+class SidePannel extends React.Component {
+  state = {
+    showModal: false,
+    percentage: 0,
+    createStatus: ""
+  };
+  serverRef = this.props.firebase.database().ref("servers");
+  storage = this.props.firebase.storage().ref("serversIcon");
 
-  const handleCreateServer = (name, file) => {
-    setStatus("prepairing");
+  handleCreateServer = (name, file) => {
+    this.setState({ createStatus: "prepairing" });
 
     //first we gonna upload the icon
-    uploadFile(file, name);
+    this.uploadFile(file, name);
     //after icon uploaded its gonna call createServer with url
   };
-  const uploadFile = (file, name) => {
+  uploadFile = (file, name) => {
     const path = uuidv4(); //generate unique filename
-    const task = storage.child(path + ".jpg").put(file); //upload it to storage
+    const task = this.storage.child(path + ".jpg").put(file); //upload it to storage
 
     //add event event listner(task.on(1,2,3,4)) to check the upload status and get download url after upload done
     // task.on accepts 4 args =>  1."event name", 2."snap on every change", 3."error", 4."done"
@@ -36,32 +37,31 @@ const SidePannel = ({
         //2. snap on every change
         var percentage =
           Math.round(snap.bytesTransferred / snap.totalBytes) * 100;
-        setPercentage(percentage);
-        setStatus("uploading icon...");
+        this.setState({ percentage, createStatus: "uploading icon..." });
       },
       err => console.log(err.message), //3. errors
       () => {
         //4. when upload is done
         //get url of icon
         task.snapshot.ref.getDownloadURL().then(url => {
-          setStatus("uploaded");
-          createServer(name, url); //create server after icon uploaded
+          this.setState({ createStatus: "uploaded" });
+          this.createServer(name, url); //create server after icon uploaded
         });
       }
     );
   };
 
-  const createServer = (name, url) => {
-    setStatus("creating Server");
+  createServer = (name, url) => {
+    this.setState({ createStatus: "creating Server" });
     //create server in database with url and name
-    const key = serverRef.push().getKey();
-    serverRef
+    const key = this.serverRef.push().getKey();
+    this.serverRef
       .child(key)
       .set({
         name,
         url,
         id: key,
-        admin: { name: user.displayName, uid: user.uid },
+        admin: { name: this.props.user.displayName, uid: this.props.user.uid },
         category: {
           general: {
             name: "general",
@@ -70,63 +70,67 @@ const SidePannel = ({
         }
       })
       .then(() => {
-        setStatus("adding server");
-        addServer(key);
+        this.setState({ createStatus: "adding server" });
+        this.addServer(key);
       })
       .catch(err => console.log(err));
   };
 
   //added created server to user data
-  const addServer = id => {
-    firebase
+  addServer = id => {
+    this.props.firebase
       .database()
       .ref("users")
-      .child(`${user.uid}/servers/${id}`)
+      .child(`${this.props.user.uid}/servers/${id}`)
       .set({
         id
       })
       .then(() => {
-        setStatus("done");
-        loadServer(id); //load the server that user has joined
-        setShowModal(false); //hide the modal
-        setStatus("");
+        this.setState({
+          createStatus: "",
+          showModal: false
+        });
+        // this.props.loadServer(id); //load the server that user has joined
       });
   };
 
-  return (
-    <div className="sidepannel">
-      <Switch
-        url="https://i.imgur.com/qMgJs45.png"
-        onClick={() => selectServer(null)}
-      />
-      <div className="underline"></div>
-
-      {/* display joined servers */}
-      {joinedServers
-        ? joinedServers.map(server => (
-            <Server
-              url={server.url}
-              key={server.id}
-              onClick={() => selectServer(server)}
-            />
-          ))
-        : ""}
-      <Add
-        onClick={() => setShowModal(true)}
-        url="https://cdn3.iconfinder.com/data/icons/stroke/53/Button-512.png"
-      />
-      {showModal ? (
-        <AddModal
-          create="Server"
-          show={showModal}
-          onClick={handleCreateServer}
-          handleClose={() => setShowModal(false)}
-          status={createStatus}
-          percentage={percentage}
+  render() {
+    const { selectServer, joinedServers } = this.props;
+    return (
+      <div className="sidepannel">
+        <Switch
+          url="https://i.imgur.com/qMgJs45.png"
+          onClick={() => selectServer(null)}
         />
-      ) : null}
-    </div>
-  );
-};
+        <div className="underline"></div>
+
+        {/* display joined servers */}
+        {joinedServers
+          ? joinedServers.map(server => (
+              <Server
+                url={server.url}
+                key={server.id}
+                onClick={() => selectServer(server)}
+              />
+            ))
+          : ""}
+        <Add
+          onClick={() => this.setState({ showModal: true })}
+          url="https://cdn3.iconfinder.com/data/icons/stroke/53/Button-512.png"
+        />
+        {this.state.showModal ? (
+          <AddModal
+            create="Server"
+            show={this.setState.showModal}
+            onClick={this.handleCreateServer}
+            handleClose={() => this.setState({ showModal: false })}
+            status={this.state.createStatus}
+            percentage={this.state.percentage}
+          />
+        ) : null}
+      </div>
+    );
+  }
+}
 
 export default SidePannel;
