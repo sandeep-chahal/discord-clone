2e26ce8cb001bf368c0390b26bd96f75c0226881
sandeep-chahal <sandeepchahal573@gmail.com>
2020-02-09 09:19:44 +0530

    just checking after reinstalling new window

--- src/Components/Discord/Discord.js
+++ src/Components/Discord/Discord.js
@@ -12,123 +12,125 @@ import ServerUsers from "./Messages/ServerInfo/ServerUsers";
 
 const extra = ["totalServers", "activity"];
 class Discord extends Component {
-  state = {};
+	state = {
+		temp: null
+	};
 
-  changeCurrentSelected = to => {
-    if (to.server) {
-      const userRole = this.props.joinedServers[to.server].users[
-        this.props.user.uid
-      ].role;
-      const roles = this.props.joinedServers[to.server].roles;
-      const role = this.props.joinedServers[to.server].roles[userRole] || null;
-      to.role = role;
-      to.roles = roles;
-    }
-    this.props.selectServer(to);
-  };
+	changeCurrentSelected = to => {
+		if (to.server) {
+			const userRole = this.props.joinedServers[to.server].users[
+				this.props.user.uid
+			].role;
+			const roles = this.props.joinedServers[to.server].roles;
+			const role = this.props.joinedServers[to.server].roles[userRole] || null;
+			to.role = role;
+			to.roles = roles;
+		}
+		this.props.selectServer(to);
+	};
 
-  getMessages = messages => {
-    try {
-      return messages[this.props.server][this.props.channel.id];
-    } catch (e) {
-      return null;
-    }
-  };
+	getMessages = messages => {
+		try {
+			return messages[this.props.server][this.props.channel.id];
+		} catch (e) {
+			return null;
+		}
+	};
 
-  render() {
-    const {
-      user,
-      joinedServers,
-      removeServer,
-      messages,
-      server,
-      channel,
-      role,
-      roles,
-      selectedDM,
-      dm,
-      dms
-    } = this.props;
-    return (
-      <div className="discord">
-        <SidePannel
-          selectedServer={server}
-          changeCurrentSelected={this.changeCurrentSelected}
-          firebase={firebase}
-          user={user}
-          joinedServers={joinedServers}
-        />
-        {server !== null ? (
-          <Channels
-            selectedServer={joinedServers[server]}
-            selectedChannel={channel}
-            removeServer={removeServer}
-            uid={user.uid}
-            changeCurrentSelected={this.changeCurrentSelected}
-            userRole={role}
-          />
-        ) : (
-          <UserPannel
-            changeCurrentSelected={this.changeCurrentSelected}
-            selectedDM={selectedDM}
-            dms={dms}
-            userUid={user.uid}
-          />
-        )}
-        {server ? (
-          <div className="messages-wrapper">
-            <Messages
-              server={joinedServers[server]}
-              messages={this.getMessages(messages)}
-              channel={channel}
-              headerName={"# " + channel.name}
-              user={user}
-              userRole={role}
-              roles={roles}
-            />
-            <ServerUsers roles={roles} users={joinedServers[server].users} />
-          </div>
-        ) : extra.includes(selectedDM) ? (
-          <Extra extra={selectedDM} />
-        ) : (
-          <div className="messages-wrapper">
-            <Messages
-              dm={{ ...dms[dm], id: dm }}
-              headerName={
-                "@ " +
-                (dms[dm].user1.uid === user.uid
-                  ? dms[dm].user2.name
-                  : dms[dm].user1.name)
-              }
-              user={user}
-            />
-          </div>
-        )}
-      </div>
-    );
-  }
+	render() {
+		const {
+			user,
+			joinedServers,
+			removeServer,
+			messages,
+			server,
+			channel,
+			role,
+			roles,
+			selectedDM,
+			dm,
+			dms
+		} = this.props;
+		return (
+			<div className="discord">
+				<SidePannel
+					selectedServer={server}
+					changeCurrentSelected={this.changeCurrentSelected}
+					firebase={firebase}
+					user={user}
+					joinedServers={joinedServers}
+				/>
+				{server !== null ? (
+					<Channels
+						selectedServer={joinedServers[server]}
+						selectedChannel={channel}
+						removeServer={removeServer}
+						uid={user.uid}
+						changeCurrentSelected={this.changeCurrentSelected}
+						userRole={role}
+					/>
+				) : (
+					<UserPannel
+						changeCurrentSelected={this.changeCurrentSelected}
+						selectedDM={selectedDM}
+						dms={dms}
+						userUid={user.uid}
+					/>
+				)}
+				{server ? (
+					<div className="messages-wrapper">
+						<Messages
+							server={joinedServers[server]}
+							messages={this.getMessages(messages)}
+							channel={channel}
+							headerName={"# " + channel.name}
+							user={user}
+							userRole={role}
+							roles={roles}
+						/>
+						<ServerUsers roles={roles} users={joinedServers[server].users} />
+					</div>
+				) : extra.includes(selectedDM) ? (
+					<Extra extra={selectedDM} />
+				) : (
+					<div className="messages-wrapper">
+						<Messages
+							dm={{ ...dms[dm], id: dm }}
+							headerName={
+								"@ " +
+								(dms[dm].user1.uid === user.uid
+									? dms[dm].user2.name
+									: dms[dm].user1.name)
+							}
+							user={user}
+						/>
+					</div>
+				)}
+			</div>
+		);
+	}
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-    removeServer: id => dispatch(removeServer(id)),
-    selectServer: id => dispatch(selectServer(id))
-  };
+	return {
+		removeServer: id => dispatch(removeServer(id)),
+		selectServer: id => dispatch(selectServer(id))
+	};
 }
 
 function mapStateToProps(state) {
-  return {
-    user: state.user.user,
-    joinedServers: state.server.joinedServers,
-    messages: state.server.messages,
-    server: state.server.server,
-    channel: state.server.channel,
-    selectedDM: state.server.dm,
-    role: state.server.role,
-    roles: state.server.roles,
-    dms: state.server.dms,
-    dm: state.server.dm
-  };
+	return {
+		user: state.user.user,
+		joinedServers: state.server.joinedServers,
+		messages: state.server.messages,
+		server: state.server.server,
+		channel: state.server.channel,
+		selectedDM: state.server.dm,
+		role: state.server.role,
+		roles: state.server.roles,
+		dms: state.server.dms,
+		dm: state.server.dm
+	};
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Discord);
