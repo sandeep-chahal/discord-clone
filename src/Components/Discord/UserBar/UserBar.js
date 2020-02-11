a98cdb755b96416477d705d4e8dee7d32935f04c
sandeep-chahal <sandeepchahal573@gmail.com>
2020-02-11 20:09:16 +0530

    added functionality to change profile pic

--- src/Components/Discord/UserBar/UserBar.js
+++ src/Components/Discord/UserBar/UserBar.js
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "./UserBar.scss";
-import firebase from "firebase";
+import firebase from "../../../firebase";
+import UserSettings from "./UserSettings";
 
 const UserBar = props => {
+	const [showSettings, setShowSettings] = useState(false);
+	const [uploading, setUploading] = useState(false);
+
 	const user = firebase.auth().currentUser;
+
+	const updateProfile = file => {
+		//uploading file
+		const task = firebase
+			.storage()
+			.ref("users/")
+			.child(user + ".jpg")
+			.put(file);
+		task.on(
+			"state_changed",
+			snap => {
+				setUploading(true);
+			},
+			err => {
+				setUploading(false);
+			},
+			() => {
+				task.snapshot.ref.getDownloadURL().then(url => {
+					//updating profile
+					firebase
+						.auth()
+						.currentUser.updateProfile({
+							photoURL: url
+						})
+						.then(() => {
+							//updating profile in database
+							firebase
+								.database()
+								.ref("users")
+								.child(user.uid + "/profile")
+								.child("avatar")
+								.set(url)
+								.then(() => {
+									setShowSettings(false);
+									setUploading(false);
+								})
+								.catch(err => setUploading(false));
+						})
+						.catch(err => setUploading(false));
+				});
+			}
+		);
+	};
 
 	return (
 		<div className="userbar">
@@ -17,8 +64,19 @@ const UserBar = props => {
 			<div>
 				<div className="icon mic-icon"></div>
 				<div className="icon headphone-icon"></div>
-				<div className="icon settings-icon"></div>
+				<div
+					className="icon settings-icon"
+					onClick={() => setShowSettings(true)}
+				></div>
 			</div>
+			{showSettings ? (
+				<UserSettings
+					updateProfile={updateProfile}
+					photo={user.photoURL}
+					uploading={uploading}
+					close={() => uploading && setShowSettings(false)}
+				/>
+			) : null}
 		</div>
 	);
 };
---
+++ src/Components/Discord/UserBar/UserSettings.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const UserSettings = props => {
+	const [file, setFile] = useState(null);
+
+	const handleSubmit = () => {
+		if (file) props.updateProfile(file);
+	};
+
+	const handleClose = e => {
+		if (e.target.classList.contains("user-settings-bg")) props.close();
+	};
+
+	return (
+		<div className="user-settings-bg" onClick={handleClose}>
+			<div className="user-settings">
+				<h2>Change Profile Picutre</h2>
+				<input
+					type="file"
+					id="file-prev"
+					onChange={e => {
+						if (e.target.files[0]) setFile(e.target.files[0]);
+					}}
+					style={{ display: "none" }}
+				/>
+				<label
+					htmlFor="file-prev"
+					className="file-prev"
+					style={{
+						backgroundImage: `url(${
+							file ? URL.createObjectURL(file) : props.photo
+						})`
+					}}
+				></label>
+				<div className="update-btn" onClick={handleSubmit}>
+					{props.uploading ? "Updating" : "Update"}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default UserSettings;
