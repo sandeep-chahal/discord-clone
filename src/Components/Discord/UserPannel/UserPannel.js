71a2ca013562528b9101eb007e5fa07ad872c1ff
sandeep-chahal <sandeepchahal573@gmail.com>
2020-03-07 20:25:33 +0530

    fixed color problem in message

--- src/Components/Discord/UserPannel/UserPannel.js
+++ src/Components/Discord/UserPannel/UserPannel.js
@@ -3,58 +3,58 @@ import "./UserPannel.scss";
 import DmUser from "./DmUser";
 
 class UserPannel extends React.Component {
-  changeSelected = to => {
-    this.props.changeCurrentSelected({
-      dm: to
-    });
-  };
+	changeSelected = to => {
+		this.props.changeCurrentSelected({
+			dm: to
+		});
+	};
 
-  displayDms = dms => {
-    if (!dms) return null;
-    const keys = Object.keys(dms);
-    return keys.map(key => {
-      const otherUser =
-        this.props.userUid === dms[key].user1.uid
-          ? dms[key].user2
-          : dms[key].user1;
-      return (
-        <DmUser
-          key={key}
-          name={otherUser.name}
-          photo={otherUser.photo}
-          active={this.props.selectedDM === key}
-          onClick={() => this.changeSelected(key)}
-        />
-      );
-    });
-  };
+	displayDms = dms => {
+		if (!dms) return null;
+		const keys = Object.keys(dms);
+		return keys.map(key => {
+			const otherUser =
+				this.props.userUid === dms[key].user1.uid
+					? dms[key].user2
+					: dms[key].user1;
+			return (
+				<DmUser
+					key={key}
+					name={otherUser.name}
+					photo={otherUser.photo}
+					active={this.props.selectedDM === key}
+					onClick={() => this.changeSelected(key)}
+				/>
+			);
+		});
+	};
 
-  render() {
-    return (
-      <div className="userpannel">
-        <div
-          className={
-            this.props.selectedDM === "totalServers" ? "active item" : "item"
-          }
-          onClick={() => this.changeSelected("totalServers")}
-        >
-          <span className="server-icon"></span>Servers
-        </div>
+	render() {
+		return (
+			<div className="userpannel">
+				<div
+					className={
+						this.props.selectedDM === "totalServers" ? "active item" : "item"
+					}
+					onClick={() => this.changeSelected("totalServers")}
+				>
+					<span className="server-icon"></span>Servers
+				</div>
 
-        <div
-          className={
-            this.props.selectedDM === "activity" ? "active item" : "item"
-          }
-          onClick={() => this.changeSelected("activity")}
-        >
-          <span className="activity-icon"></span>activity
-        </div>
+				<div
+					className={
+						this.props.selectedDM === "activity" ? "active item" : "item"
+					}
+					onClick={() => this.changeSelected("activity")}
+				>
+					<span className="activity-icon"></span>activity
+				</div>
 
-        <h3>DIRECT MESSAGE</h3>
-        {this.displayDms(this.props.dms)}
-      </div>
-    );
-  }
+				<h3>DIRECT MESSAGE</h3>
+				{this.displayDms(this.props.dms)}
+			</div>
+		);
+	}
 }
 
 export default UserPannel;
